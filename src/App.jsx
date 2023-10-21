c1bef6ff9b20ac7e9c78f497b62a62985c3144f6
christian_reynaltt <creynaltt@maristak.net>
2023-10-21 12:13:58 +0200

    formulario controlado con validaciones

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,38 @@
 import "./App.css";
-import { useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import Movies from "./components/Movies";
 import useMovies from "./hooks/useMovies";
 
 function App() {
 
   const { movies } = useMovies()
+  const [query, setQuery] = useState("")
+  const [error, setError] = useState(null)
+
+  useEffect(() => {
+    if (query === "") {
+      setError("No se puede buscar una pelicula vacia.")
+      return
+    }
+
+    if (query.length < 3) {
+      setError("La película debe de tener al menos 3 carácteres.")
+      return
+    }
+
+    setError(null)
+  }, [query])
+
 
   const handleSubmit = (ev) => {
     ev.preventDefault()
     const { query } = Object.fromEntries(
       new window.FormData(ev.target)
     )
-    console.log(query)
+  }
+
+  const handleChange = (ev) => {
+    setQuery(ev.target.value)
   }
 
   return (
@@ -20,9 +40,10 @@ function App() {
       <header>
         <h1>Buscador de películas</h1>
         <form className="form" onSubmit={handleSubmit}>
-          <input name="query" placeholder="Avengers, Star wars, The Matix..." />
+          <input value={query} onChange={handleChange} name="query" placeholder="Avengers, Star wars, The Matix..." />
           <button type="submit" >Buscar</button>
         </form>
+        {error && <p style={{ color: "red" }}>{error}</p>}
       </header>
 
       <main>
